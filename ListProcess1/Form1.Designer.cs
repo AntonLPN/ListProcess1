816bef1093946f1dac45a3b791c72668b7e14a38
AntonLPN <68461689+AntonLPN@users.noreply.github.com>
2021-02-16 12:19:13 +0200

    Add project files.

---
+++ ListProcess1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace ListProcess1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(56, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ListProcess Single load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(267, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ListProcess  periodically";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(56, 104);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(401, 302);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "NameProcess";
+            this.columnHeader1.Width = 220;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "VirtualMemorySize64";
+            this.columnHeader2.Width = 254;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(516, 103);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(459, 303);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(634, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Info about selected item";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ListProcess1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListProcess1
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        Process[] processRefresh;
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += Timer_Tick;
+         processRefresh = Process.GetProcesses();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+           
+            processRefresh = Process.GetProcesses();
+            listView1.Refresh();//обновляем лист
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+            Process[] processes = Process.GetProcesses();//получаем список процессов 
+           
+            ListViewItem lstViewItems = null;
+        
+            foreach (Process process in processes)
+            {
+              
+                lstViewItems = new ListViewItem();
+                lstViewItems.Text = process.ProcessName;//добавляем название процесса
+                //получаем память занятую процессом
+                lstViewItems.SubItems.Add(Process.GetProcessesByName(process.ProcessName)[0].VirtualMemorySize64.ToString());
+
+              listView1.Items.Add(lstViewItems);
+            }
+             
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+           
+            ListViewItem lstViewItems = null;
+            
+            foreach (Process process in processRefresh)
+            {
+                lstViewItems = new ListViewItem();
+                lstViewItems.Text = process.ProcessName;
+
+                lstViewItems.SubItems.Add(Process.GetProcessesByName(process.ProcessName)[0].VirtualMemorySize64.ToString());
+
+
+                listView1.Items.Add(lstViewItems);
+            }
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+         
+            for (int i = 0; i < listView1.SelectedItems.Count; i++)
+            {
+                if (listView1.SelectedItems[i].Selected)
+                {
+                    try
+                    {
+                        string nameProcess = listView1.SelectedItems[i].Text;
+                  Process process = Process.GetProcessesByName(nameProcess)[0];
+                    this.listBox1.Items.Add($"Name Process : {process.ProcessName}");
+                    this.listBox1.Items.Add($"Identity Process : {process.Id}");                   
+                        this.listBox1.Items.Add($"Time Start Process : {process.StartTime}");
+                        this.listBox1.Items.Add($"Counter of threads : {process.Threads.Count}");
+                        this.listBox1.Items.Add($"Processor Time : {process.TotalProcessorTime}");
+                        this.listBox1.Items.Add($"Counter process copy : {Process.GetProcessesByName(process.ProcessName).Count()}");
+                    }
+                    catch (Exception ex)
+                    {
+
+                        
+                    }
+                   
+                 
+                }
+            }
+           
+
+        
+
+        }
+    }
+
+
+    
+}
